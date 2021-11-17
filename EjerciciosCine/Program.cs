c55b85677c9655470f917c5e176ab4927af543c0
CornejoRoberto <roberto_samp@outlook.com>
2021-11-17 15:08:40 -0500

    Agregar archivos de proyecto.

---
+++ EjerciciosCine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjerciciosCine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
